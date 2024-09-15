c9928d51c4e2cf978f174d3b99654583d08c234b
Srivasu-U <suvasista2@gmail.com>
2024-09-15 17:38:00 +0530

    Modifying lexer to use more symbols and keywords

--- monkey-v1/lexer/lexer_test.go
+++ monkey-v1/lexer/lexer_test.go
@@ -6,20 +6,115 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	var input string = `=+(){},;`
+	var input string = `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x+y;
+	};
+	let result = add(five, ten);
+
+	!-*/5;
+	5 < 10 > 5;
+	5 == 5;
+	5 != 10;
+	5 <= 10;
+	10 >= 5;
+	true;
+	false;
+	if;
+	else;
+	return;
+	`
 
 	tests := []struct {
 		expectedType token.TokenType
 		expectedLit  string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.ASTERISK, "*"},
+		{token.SLASH, "/"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.EQ, "=="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT_EQ, "<="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.GT_EQ, ">="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.SEMICOLON, ";"},
+		{token.ELSE, "else"},
+		{token.SEMICOLON, ";"},
+		{token.RETURN, "return"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
--- monkey-v1/token/token.go
+++ monkey-v1/token/token.go
@@ -16,8 +16,20 @@ const (
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+	LT_EQ  = "<="
+	GT_EQ  = ">="
 
 	// Delimiters
 	COMMA     = ","
@@ -30,4 +42,26 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookUpIdent(ident string) TokenType { // Basically return keyword type, if it is a keyword, else return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
