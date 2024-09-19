9e46071e4bc76fd15f9c0e7ffe3e45ac0d2119f7
Srivasu-U <suvasista2@gmail.com>
2024-09-19 12:44:39 +0530

    Fleshing out the parser with error logging

--- monkey-v2/parser/parser.go
+++ monkey-v2/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"Learning-Go/monkey-v2/ast"
 	"Learning-Go/monkey-v2/lexer"
 	"Learning-Go/monkey-v2/token"
+	"fmt"
 )
 
 /*
@@ -17,10 +18,14 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens to set curToken and peekToken.
 	p.nextToken()
@@ -29,11 +34,84 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) parseStatement() ast.Statement {
+	// This method returns an actual Statement struct but parseLetStatement()
+	// returns a pointer. Why? What is the convention?
+
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) curTokenIs(tokType token.TokenType) bool {
+	return p.curToken.Type == tokType
+}
+
+func (p *Parser) peekTokenIs(tokType token.TokenType) bool {
+	return p.peekToken.Type == tokType
+}
+
+func (p *Parser) expectPeek(tokType token.TokenType) bool {
+	// Function to enforce the order correctness by checking next token
+
+	if p.peekTokenIs(tokType) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(tokType)
+		return false
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	//TODO: Expression handling
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
 }
