ed89ae4c76e6cfebce9e10d2fb804ad32dae373a
Srivasu-U <suvasista2@gmail.com>
2024-09-22 11:22:55 +0530

    Initial commit for monkey-v3

---
+++ monkey-v3/src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"Learning-Go/monkey-v2/src/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to start typing commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ monkey-v3/src/repl/repl.go
@@ -0,0 +1,43 @@
+package repl
+
+import (
+	"Learning-Go/monkey-v2/src/lexer"
+	"Learning-Go/monkey-v2/src/parser"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
