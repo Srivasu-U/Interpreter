2b05ea46aa6987210ac7ec4c37cc9ae76410494c
Srivasu-U <suvasista2@gmail.com>
2024-09-28 22:26:14 +0530

    Lexer support for hashes

--- monkey-v4/src/lexer/lexer_test.go
+++ monkey-v4/src/lexer/lexer_test.go
@@ -27,6 +27,7 @@ func TestNextToken(t *testing.T) {
 	"foobar"
 	"foo bar"
 	[1, 3];
+	{"foo": "bar"}
 	`
 
 	tests := []struct {
@@ -127,6 +128,12 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "3"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
 
 		{token.EOF, ""},
 	}
