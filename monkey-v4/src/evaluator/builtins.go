e42db8a3b05456436642ccae6ac4bf0c18516fd0
Srivasu-U <suvasista2@gmail.com>
2024-09-29 18:31:30 +0530

    Removing redundant type

--- monkey-v4/src/evaluator/builtins.go
+++ monkey-v4/src/evaluator/builtins.go
@@ -104,7 +104,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"puts": &object.Builtin{
+	"puts": {
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
 				fmt.Println(arg.Inspect())
