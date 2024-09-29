a9ff6117721dec2ec3595ec18734c56a9fd045a9
Srivasu-U <suvasista2@gmail.com>
2024-09-29 18:12:31 +0530

    Evaluating Hash Literals

--- monkey-v4/src/object/object_test.go
+++ monkey-v4/src/object/object_test.go
@@ -7,6 +7,10 @@ func TestStringHashKey(t *testing.T) {
 	hello2 := &String{Value: "Hello World"}
 	diff1 := &String{Value: "My name is johnny"}
 	diff2 := &String{Value: "My name is johnny"}
+	boolean1 := &Boolean{Value: true}
+	boolean2 := &Boolean{Value: true}
+	number1 := &Integer{Value: 100}
+	number2 := &Integer{Value: 100}
 
 	if hello1.HashKey() != hello2.HashKey() {
 		t.Errorf("strings with same content have different hash keys")
@@ -14,6 +18,14 @@ func TestStringHashKey(t *testing.T) {
 
 	if diff1.HashKey() != diff2.HashKey() {
 		t.Errorf("strings with same content have different hash keys")
+	}
+
+	if boolean1.HashKey() != boolean2.HashKey() {
+		t.Errorf("booleans with same content have different hash keys")
+	}
+
+	if number1.HashKey() != number2.HashKey() {
+		t.Errorf("integers with same content have different hash keys")
 	}
 
 	if hello1.HashKey() == diff1.HashKey() {
